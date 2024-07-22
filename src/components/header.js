7e3b3cf3189bb8b7e41c53c4704f511439d7414e
ahmadb009 <ahmadbacchus0@gmail.com>
2024-07-22 01:07:41 -0400

    added the original files

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <header className="header">
+        <nav>
+            <ul>
+                <li><a>href="#planets"</a></li>
+                <li><a>href="#astronauts"</a></li>
+                <li><a>href="#missions"</a></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default Header;
